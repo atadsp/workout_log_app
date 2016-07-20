b06547519bc302a9aa52582f78290594ad2ff531
Chris Beckman <atadsp@gmail.com>
2016-07-20 14:58:28 -0400

    user registrations beta added

---
+++ server/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var sequelize = require('./db.js');
+
+//usermodel
+
+sequelize.sync();
+
+app.use(bodyParser.json());
+
+app.use(require('./middleware/headers'));
+
+app.use('/api/user', require('./routes/user'));
+
+app.use('/api/test', function(req,res){
+	res.send("I'm back losers");
+});
+
+app.listen(3000, function(){
+	console.log("app is listening on port 3000");
+});
